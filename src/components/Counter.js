66d92a6b5ca5c251ca4c6143c742cd137cd87924
Tatsuya Inokuchi <ino1221@gmail.com>
2020-01-19 14:36:32 +0900

    Counter Component　を functional componentで実装してみる

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,44 +1,70 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
-class Counter extends React.Component {
+const Counter = props => {
+    const [count, setCount] = useState(0)
+    const { title, onChange } = props
 
-    state = {
-        count: 0
+    const increment = () => {
+        const newCount = count + 1
+        setCount(newCount)
+        onChange('increment', newCount)
     }
 
-    componentDidUpdate(prevProps, prevState) {
-        // console.log(prevState)
-        // console.log(prevProps)
+    const decrement = () => {
+        const newCount = count - 1
+        setCount(newCount)
+        onChange('decrement', newCount)
     }
 
-    increment = () => {
-        const { count } = this.state
-        const { onChange } = this.props
-        this.setState({count: count + 1})
-        onChange('increment', count + 1)
-    }
-
-    decrement = () => {
-        const { count } = this.state
-        const { onChange } = this.props
-        this.setState({count: count - 1})
-        onChange('decrement', count - 1)
-    }
-
-
-
-    render() {
-        const { count } = this.state
-        const { title } = this.props
-        return (
-            <>
-                <h1>{title}</h1>
-                <button onClick={this.increment}>Increment</button>
-                <div className="counter">{count}</div>
-                <button onClick={this.decrement}>Decrement</button>
-            </>
-        )
-    }
+    return (
+        <div>
+            <h1>{title}</h1>
+            <button onClick={increment}>Increment</button>
+            <div className="counter">{count}</div>
+            <button onClick={decrement}>Decrement</button>
+        </div>
+    )
 }
+
+// class Counter extends React.Component {
+//
+//     state = {
+//         count: 0
+//     }
+//
+//     componentDidUpdate(prevProps, prevState) {
+//         // console.log(prevState)
+//         // console.log(prevProps)
+//     }
+//
+//     increment = () => {
+//         const { count } = this.state
+//         const { onChange } = this.props
+//         this.setState({count: count + 1})
+//         onChange('increment', count + 1)
+//     }
+//
+//     decrement = () => {
+//         const { count } = this.state
+//         const { onChange } = this.props
+//         this.setState({count: count - 1})
+//         onChange('decrement', count - 1)
+//     }
+//
+//
+//
+//     render() {
+//         const { count } = this.state
+//         const { title } = this.props
+//         return (
+//             <>
+//                 <h1>{title}</h1>
+//                 <button onClick={this.increment}>Increment</button>
+//                 <div className="counter">{count}</div>
+//                 <button onClick={this.decrement}>Decrement</button>
+//             </>
+//         )
+//     }
+// }
 
 export default Counter
