819179cf3fb9b0a865efc3754b8296206d43a076
Tatsuya Inokuchi <ino1221@gmail.com>
2020-01-19 11:14:19 +0900

    同じ実装をcomponentで実装 その際にアロー関数と通常関数との渡し方の違いを検証

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,70 +1,84 @@
 import React, { useState, useEffect } from 'react'
 import './Home.css'
 
-const Home = () => {
-
-    const [message, setMessage] = useState('Super Message!!!')
-    const [count, setCount] = useState(0)
-
-    useEffect(() => {
-        setTimeout(() => {
-            setMessage('I am updated Message')
-        }, 1000)
-    }, [])
-
-    const increment = () => {
-        setCount(count + 1)
-    }
-
-    const decrement = () => {
-        setCount(count - 1)
-    }
-
-    return (
-        <div className="container">
-            <h1>I am Home Page</h1>
-            <p>{message}</p>
-            <button onClick={increment}>Increment</button>
-            <div className="counter">{count}</div>
-            <button onClick={decrement}>Decrement</button>
-        </div>
-    )
-}
-
-// class Home extends React.Component {
+// const Home = () => {
 //
-//     constructor(props) {
-//         super(props);
+//     const [message, setMessage] = useState('Super Message!!!')
+//     const [count, setCount] = useState(0)
 //
-//         this.state = {
-//             message: 'Super Message!!!'
-//         }
-//
-//     }
-//
-//     // state = {
-//     //     message: 'Super Message!!!!!'
-//     // }
-//
-//     componentDidMount() {
+//     useEffect(() => {
 //         setTimeout(() => {
-//             this.setState({message: 'I am updated Message'})
+//             setMessage('I am updated Message')
 //         }, 1000)
+//     }, [])
+//
+//     const increment = () => {
+//         setCount(count + 1)
 //     }
 //
-//
-//
-//
-//     render() {
-//         const { message } = this.state;
-//         return (
-//             <div className="container">
-//                 <h1>I am Home Page</h1>
-//                 <p>{message}</p>
-//             </div>
-//         )
+//     const decrement = () => {
+//         setCount(count - 1)
 //     }
+//
+//     return (
+//         <div className="container">
+//             <h1>I am Home Page</h1>
+//             <p>{message}</p>
+//             <button onClick={increment}>Increment</button>
+//             <div className="counter">{count}</div>
+//             <button onClick={decrement}>Decrement</button>
+//         </div>
+//     )
 // }
+
+class Home extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            message: 'Super Message!!!',
+            count: 0
+        }
+
+    }
+
+    // state = {
+    //     message: 'Super Message!!!!!'
+    // }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({message: 'I am updated Message'})
+        }, 1000)
+    }
+
+    increment = () => {
+        const { count } = this.state
+        this.setState({count: count + 1})
+    }
+
+    decrement() {
+        const { count } = this.state
+        this.setState({count: count - 1})
+    }
+
+
+
+
+    render() {
+        const { message, count } = this.state;
+        return (
+            <div className="container">
+                <h1>I am Home Page</h1>
+                <p>{message}</p>
+                <button onClick={this.increment}>Increment</button>
+                <div className="counter">{count}</div>
+                <button onClick={() => this.decrement()}>Decrement</button>
+            </div>
+        )
+    }
+}
 
 
 
