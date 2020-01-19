7f97e238a4624cf98efca20290442a55f9fc6938
Tatsuya Inokuchi <ino1221@gmail.com>
2020-01-19 10:56:06 +0900

    component型と同じ機能を functionで実装

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,49 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Home.css'
 
-// const Home = () => {
-//
-//     return (
-//         <div className="container">
-//             <h1>I am Home Page</h1>
-//         </div>
-//     )
-// }
+const Home = () => {
 
-class Home extends React.Component {
+    const [message, setMessage] = useState('Super Message!!!')
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            message: 'Super Message!!!'
-        }
-
-    }
-
-    // state = {
-    //     message: 'Super Message!!!!!'
-    // }
-
-    componentDidMount() {
+    useEffect(() => {
         setTimeout(() => {
-            this.setState({message: 'I am updated Message'})
+            setMessage('I am updated Message')
         }, 1000)
-    }
+    }, [])
 
-
-
-
-    render() {
-        const { message } = this.state;
-        return (
-            <div className="container">
-                <h1>I am Home Page</h1>
-                <p>{message}</p>
-            </div>
-        )
-    }
+    return (
+        <div className="container">
+            <h1>I am Home Page</h1>
+            <p>{message}</p>
+        </div>
+    )
 }
+
+// class Home extends React.Component {
+//
+//     constructor(props) {
+//         super(props);
+//
+//         this.state = {
+//             message: 'Super Message!!!'
+//         }
+//
+//     }
+//
+//     // state = {
+//     //     message: 'Super Message!!!!!'
+//     // }
+//
+//     componentDidMount() {
+//         setTimeout(() => {
+//             this.setState({message: 'I am updated Message'})
+//         }, 1000)
+//     }
+//
+//
+//
+//
+//     render() {
+//         const { message } = this.state;
+//         return (
+//             <div className="container">
+//                 <h1>I am Home Page</h1>
+//                 <p>{message}</p>
+//             </div>
+//         )
+//     }
+// }
 
 
 
