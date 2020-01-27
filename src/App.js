cfce2f3d3d182774a162dafd636ba40dacb5d7f6
Tatsuya Inokuchi <ino1221@gmail.com>
2020-01-28 01:54:05 +0900

    scssのインポートと、パスの書き方 それにcssの適用方法

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import HomePage from './pages/Home'
+import Sidebar from 'components/Sidebar'
 
 function App() {
   return (
     <div>
       <HomePage/>
+      <Sidebar />
     </div>
   );
 }
