af3d956934053163e78dbbe67c424479409994fd
Tatsuya Inokuchi <ino1221@gmail.com>
2020-02-08 15:36:44 +0900

    react-router-domのLink機能を使ってリンクを作成

---
+++ src/pages/Login.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Login = () => <h1>This is Login page</h1>
+
+export default Login
---
+++ src/pages/Register.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Register = () => <h1>This is Register Page.</h1>
+
+export default Register
