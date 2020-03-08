5dc8f331859d56b9b236fdeec3c13945addc28b2
Tatsuya Inokuchi <ino1221@gmail.com>
2020-03-08 17:37:05 +0900

    loopしてるデータにkeyの要素がなかったので、firebaseからidを与えるように変更

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,7 +9,7 @@ export const fetchServices = () => {
     .collection('services')
     .get()
     .then(snapshot => {
-      const services = snapshot.docs.map(doc => doc.data())
+      const services = snapshot.docs.map(doc => ({id: doc.id, ...doc.data()}))
       return {
         type: FETCH_SERVICES_SUCCESS,
         services
