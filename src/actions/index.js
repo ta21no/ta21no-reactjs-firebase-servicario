6a0f0d3c7d72bcc50f94d44e4de725ae25d7d5ee
Tatsuya Inokuchi <ino1221@gmail.com>
2020-07-01 02:24:20 +0900

    同じIDの検索時にキャッシュからデータを取得するように変更

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,8 +16,11 @@ export const fetchServices = () => dispatch => {
 }
 
 
-export const fetchServiceById = serviceId => dispatch => {
-  dispatch({type: FETCH_SERVICE_SUCCESS, service: {}})
+export const fetchServiceById = serviceId => (dispatch, getState) => {
+
+  const lastService = getState().selectedService.item
+  if (lastService.id && lastService.id === serviceId) {return Promise.resolve()}
+
   dispatch({type: REQUEST_SERVICE})
   return api
     .fetchServiceById(serviceId)
